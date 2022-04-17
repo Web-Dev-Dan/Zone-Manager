78d2a52c4e194ba77129d077d95e02c66d41311d
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-04-17 11:40:15 +0900

    Added dynamic date in Footer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// ----- 📆 Update Date (Year) 📆 -----
+function updateYear() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
 
+    const currentYearText = document.getElementById('currentYear');
+    currentYearText.textContent = currentYear;
+}
+
+updateYear();
