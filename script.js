d6ee177c4eae57d45299ade72bf8ec9e171c8df7
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-04-25 21:52:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,3 @@ function updateYear() {
 }
 
 updateYear();
-
-
-
-// 👨‍💻 User Profile Notification Bubble
-const closeProfileNotificationBtn = document.getElementById('closeProfileNotificationBtn');
-const profileNotificationBubble = document.getElementById('profileNotificationBubble');
-
-closeProfileNotificationBtn.addEventListener('click', closeProfileNotificationBubble);
-
-function closeProfileNotificationBubble() {
-    profileNotificationBubble.classList.add('element-hidden');
-}
